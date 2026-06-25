4b687e1bb2019418e35e910415961d5c84496729
Sudipto Banerjee <sudiz.7052@gmail.com>
2026-06-25 22:16:47 +0000

    Fixed bug in kernel - used atomic add
    - Normal additional causes incorrect value b/c result is shared across all threads - read-write-modify race condition
    - Made function templatized - make it easier to unit test

--- sum.cu
+++ sum.cu
@@ -4,38 +4,55 @@
 #include <cuda_runtime.h>
 
 
-#define N (1 << 20)
+// #define N (1 << 20)
+#define N 200
 
 
 __global__ void sum_reduction(const float* A, float* result, int n) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if(i < n) {
-        // atomicAdd(result, A[i]);
-        *result += A[i];
+        atomicAdd(result, A[i]);
     }
 }
 
-int main() {
-    float *A = (float*) malloc(sizeof(float) * N);
+
+template <typename T>
+T run_sum_reduction(int offset) {
+    T *A = (T*) malloc(sizeof(T) * N);
     for(int i=0;i<N;i++) {
-        A[i] = i + 1;
+        A[i] = i + offset;
     }
 
-    float* dA;
-    float* dResult;
+    T* dA;
+    T* dResult;
 
-    cudaMalloc(&dA, sizeof(float) * N);
-    cudaMalloc(&dResult, sizeof(float));
+    cudaMalloc(&dA, sizeof(T) * N);
+    cudaMalloc(&dResult, sizeof(T));
 
-    cudaMemcpy(dA, A, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(dA, A, sizeof(T) * N, cudaMemcpyHostToDevice);
 
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
     sum_reduction<<<blocksPerGrid, threadsPerBlock>>>(dA, dResult, N);
 
-    float result;
-    cudaMemcpy(&result, dResult, sizeof(float), cudaMemcpyDeviceToHost);
+    T result;
+    cudaMemcpy(&result, dResult, sizeof(T), cudaMemcpyDeviceToHost);
 
-    std::cout << result << std::endl;
+    return result;
+}
+
+int main() {
+    int offset = 1;
+    float result = run_sum_reduction<float>(1);
+
+    // Test correctness
+    float reference = 0.0;
+    for(int i=0;i<N;i++) {
+        reference += i + offset;
+    }
+
+    std::cout << "Reference value - " << reference << std::endl;
+    std::cout << "Computed value - " << result << std::endl;
+    return 0;
 }
